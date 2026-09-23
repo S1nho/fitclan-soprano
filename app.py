62515a074d76575aa0f8e146f11370838593d47b
S1nho <yout135live@gmail.com>
2026-09-24 00:23:54 +0300

    drop2

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET'])
-def hello():
-    return jsonify({'message': 'Hello World'})
+@app.route("/")
+def index():
+    return render_template("abonements.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
